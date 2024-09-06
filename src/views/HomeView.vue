1443fa0aa071fc44be8e2d458ca94af663ff624b
army <army.naty@gmail.com>
2024-09-06 04:10:55 -0400

    terminado

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <SingleCard />
+</template>
+
+<script>
+// @ is an alias to /src
+import SingleCard from '@/components/SingleCard.vue'
+
+export default {
+  components: {
+    SingleCard
+  }
+}
+</script>
